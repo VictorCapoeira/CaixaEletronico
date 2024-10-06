31d40906777161b5c47b1ceeff470fec37dde492
Victor Emanuel <victorgit28@outlook.com>
2024-10-06 08:53:13 -0300

    Criando metodo para verificar a conta

--- caixa/Program.cs
+++ caixa/Program.cs
@@ -13,5 +13,15 @@
 class Program{
     caixa conta1 = new caixa(1500, "Corrente", 1);
     caixa conta2 = new caixa(1000, "Salario", 2);
+    static void vefConta(caixa caixa){
+        Console.WriteLine($"ID da conta: {caixa.id}");
+        Console.WriteLine($"Tipo de conta: {caixa.conta}");
+        Console.WriteLine($"Saldo disponivel: {caixa.saldo}");
+    }
+    static void saque(caixa caixa){
+        Console.WriteLine("Informe o valor do saque: ");
+        
+    }
+
 }
 
