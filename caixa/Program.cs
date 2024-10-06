8b76a53bebc17ae1ba9936f2560827bd234ac00e
Victor Emanuel <victorgit28@outlook.com>
2024-10-06 08:43:47 -0300

    Criando objetos para teste

--- caixa/Program.cs
+++ caixa/Program.cs
@@ -1,10 +1,17 @@
 ﻿public class caixa{
     public double saldo {get; set;}
     public string conta {get; set;}
+    public int id {get;}
 
-    public caixa(double s, string c){
+    public caixa(double s, string c, int i){
         saldo = s;
         conta = c;
+        id = id;
     }
+}
+
+class Program{
+    caixa conta1 = new caixa(1500, "Corrente", 1);
+    caixa conta2 = new caixa(1000, "Salario", 2);
 }
 
