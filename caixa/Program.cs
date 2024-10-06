cc1884e6c2432745720ae913e4ae148bba451c8a
Victor Emanuel <victorgit28@outlook.com>
2024-10-06 08:33:44 -0300

    Criando classse

---
+++ caixa/Program.cs
@@ -0,0 +1,10 @@
+﻿public class caixa{
+    public double saldo {get; set;}
+    public string conta {get; set;}
+
+    public caixa(double s, string c){
+        saldo = s;
+        conta = c;
+    }
+}
+
